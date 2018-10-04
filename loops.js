b2e462ae944e4d79580e38f2bb76b87dee2ad3a4
Anandha Sudhan <rasudhan@gmail.com>
2018-10-04 22:00:59 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -21,4 +21,12 @@ function doWhileLoop() {
   function incrementVariable() {
     i = i + 1;
   }
+
+  do {
+		console.log('array.length = ' + array.length + ' and i = ' + i);
+		array = array.slice(1);
+		incrementVariable();
+	} while (array.length > 0 && i < 5);
+
+	return array;
 }

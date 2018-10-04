977f1feb6507ac05772ab33aaf7558aeb6e8bd92
Anandha Sudhan <rasudhan@gmail.com>
2018-10-04 21:55:09 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,11 @@
 function forLoop(array) {
   for(let i=0;i<25;i++) {
 
-    i==1? array[i]="I am 1 strange loop." : array[i]="I am ${i} strange loops."
+    if (i==1) {
+      array[i]="I am 1 strange loop." 
+    } else {
+      array[i]="I am ${i} strange loops."
+    } 
   }
 }
 

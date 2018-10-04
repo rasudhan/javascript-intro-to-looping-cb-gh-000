badce8d5bef7b1cc4bb5ec0225ede5a5ba7386ca
Anandha Sudhan <rasudhan@gmail.com>
2018-10-04 21:52:51 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,13 @@
+function forLoop() {
+  for(let i=0;i<25;i++) {
+    
+    i==1? array.push("I am 1 strange loop.") : array.push("I am ${i} strange loops.")
+  }
+}
+
+var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+}
+ 
